68cb6675fee540c7aef0db6265b04afbe6a841a1
DabToms <51459629+DabToms@users.noreply.github.com>
2022-11-17 17:59:15 +0100

    gh

---
+++ ChessForms/ChessForms/Decorators/TransformatedDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessForms.Figures;
+
+namespace ChessForms.Decorators;
+public class TransformatedDecorator : AbstractDecorator
+{
+    public TransformatedDecorator(IFigure fig) : base(fig)
+    {
+    }
+    public override void draw(Graphics g)
+    {
+        g.Transform = new Matrix();
+        g.TranslateTransform(ChessboardForms.ZEROX,ChessboardForms.ZEROY);
+        g.ScaleTransform(1,1);
+        base.draw(g);
+    }
+}
