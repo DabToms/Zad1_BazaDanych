0162604ec1176ca8782e7dd62f8a4dd8e98c72eb
Tomasz Dąbrowski <tomasz.dabrowski@aquard.pl>
2022-11-28 12:36:11 +0100

    fdsgsdgdsf

---
+++ Nowy folder/ChessForms/AbstractFigure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessForms.Figures;
+
+namespace ChessForms;
+public abstract class AbstractFigure : IFigure
+{
+    public static readonly int TILESIZE = 32;
+    protected int index, x, y;
+
+    public AbstractFigure(int idx, int xx, int yy)
+    {
+        index = idx;
+        x = xx;
+        y = yy;
+    }
+
+    public abstract void draw(Graphics g);
+
+    public virtual int GetX()
+    {
+        return x;
+    }
+
+    public virtual int getY()
+    {
+        return y;
+    }
+
+    public virtual void moveTo(int xx, int yy)
+    {
+        x = xx;
+        y = yy;
+    }
+
+    public IFigure Unbox()
+    {
+        return this;
+    }
+}
---
+++ Nowy folder/ChessForms/Decorators/AbstractDecorator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessForms.Figures;
+
+namespace ChessForms.Decorators;
+public class AbstractDecorator : IFigure
+{
+    private IFigure Figure;
+    public AbstractDecorator(IFigure fig)
+    {
+        Figure = fig;
+    }
+
+    public virtual void draw(Graphics g)
+    {
+        Figure.draw(g);
+    }
+
+    public virtual int GetX()
+    {
+        return Figure.GetX();
+    }
+
+    public virtual int getY()
+    {
+        return Figure.GetX();
+    }
+
+    public virtual void moveTo(int xx, int yy)
+    {
+        Figure.moveTo(xx, yy);
+    }
+
+    public virtual IFigure Unbox()
+    {
+        return Figure.Unbox();
+    }
+}
